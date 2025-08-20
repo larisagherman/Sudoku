8f1bf4437b10641c1d124e6bcb819f741adf5413
Gherman Larisa <gherman.larisa3@gmail.com>
2025-08-20 17:09:03 +0300

    done a given sudoku game with error telling

--- sudokuBoard.js
+++ sudokuBoard.js
@@ -1,5 +1,6 @@
-let selectedNumber=null
-let selectedSquare=null
+let numberSelected=null
+let selectedTile=null
+let errors=0;
 
 let board=[
     "600504039",
@@ -12,6 +13,17 @@ let board=[
     "049082170",
     "780105002"
 ]
+let solvedBoard=[
+    "672514839",
+    "194836725",
+    "358729614",
+    "916453287",
+    "437268951",
+    "825971346",
+    "261347598",
+    "549682173",
+    "783195462"
+];
 window.onload = function(){
     setGame();
 }
@@ -25,6 +37,51 @@ function setGame(){
         number.innerText=i
         number.classList.add('number')
         document.getElementById("digits").appendChild(number)
+
+        number.addEventListener("click", selectNumber)
+    }
+    for(let r=0;r<9;r++){
+        for(let c=0;c<9;c++){
+            let tile=document.createElement('div');
+            tile.id=r.toString()+"-"+c.toString();
+            if(board[r][c]!="0"){
+                tile.innerText=board[r][c]
+                tile.classList.add('tile-start')
+            }
+            if(r==2 || r==5){
+                tile.classList.add('horizontal-line')
+            }
+            if(c==2 || c==5){
+                tile.classList.add('vertical-line')
+            }
+            tile.classList.add('tile');
+            document.getElementById("board").appendChild(tile)
+
+            tile.addEventListener("click", selectTile)
+        }
+    }
+}
+function selectNumber(){
+    if(numberSelected!==null){
+        numberSelected.classList.remove('number-selected');
+    }
+    numberSelected=this
+    numberSelected.classList.add('number-selected')
+}
+function selectTile(){
+    if (numberSelected !== null) {
+        if (this.innerText != "") {
+            return;
+        }
+        let coords = this.id.split('-');
+        let r = coords[0];
+        let c = coords[1];
+        if (solvedBoard[r][c] == numberSelected.id) {
+            this.innerText = numberSelected.id;
+        } else {
+            errors += 1
+            document.getElementById("errors").innerText = errors;
+        }
     }
 }
 
